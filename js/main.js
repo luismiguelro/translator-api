d18391299f041097829fb60ef24348fed60fc588
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-02-08 15:30:26 -0500

    sending request to API (MyMemory), and fetchiing data of entered text from API

--- js/main.js
+++ js/main.js
@@ -80,12 +80,17 @@ fetch(GET_URL,OPTIONS)
 /*---------------------------------VERSION 2 ---------------*/
 
 // Tags
-const selectTag = document.querySelectorAll("select");
+const selectTag = document.querySelectorAll("select"),
+      translateBtn= document.querySelector("button"),
+      fromText = document.querySelector('.from-text'),
+      ToText = document.querySelector('.to-text');
 
 // elementos dDOM
 let translateFrom = document.querySelector('.select-from');
 let translateTo = document.querySelector('.select-to');
-console.log(translateFrom);
+
+// Valores predeterminados
+
 
 //Consultar lista de lenguajes desde el servidor
 const GET_URL = 'https://text-translator2.p.rapidapi.com/getLanguages';
@@ -105,69 +110,25 @@ fetch(GET_URL,OPTIONS)
     .then(res=>res.json())
     .then(objeto =>{
         let languages = objeto.data.languages;
-        //console.log()
+        //console.log(languages)
         
         //Codigo necesario para cargar el select
         languages.forEach(element => {
-            translateFrom.innerHTML += `<option value="${element.code}">${element.name}</option>`
+            translateFrom.innerHTML +=`<option value="${element.code}">${element.name}</option>`
             translateTo.innerHTML += `<option value="${element.code}">${element.name}</option>`
         });
-        /*
         
-        // tomar valor del translate from
-        translateFrom.addEventListener('click',()=>{
-                //console.log(translateFrom.value);
-                source_language=translateFrom.value;
-            });
+}).catch(error => console.log(error));
 
-            // tomar valor del translate to
-            translateTo.addEventListener('click',()=>{
-                //console.log(translateTo.value);
-                target_language = translateTo.value;
-            });
+translateBtn.addEventListener('click',()=>{
+    let text = fromText.value,
+    source_language=translateFrom.value;
+    target_language = translateTo.value;
+    console.log(text, target_language,target_language,translateFrom[0].value)
 
-        }).catch(error => console.log(error));
+    let URL_API = `https://api.mymemory.translated.net/get?q=${text}&langpair=${source_language}|${target_language}`
 
-        // almacenar valores a traducir
-        let source_language = 'es';
-        let target_language = 'af'
-
-        //mostrar resultado
-        let translateOut = document.querySelector('#output-translate');
-        //Obtener datos del input para enviar al servidor
-        let translate = document.querySelector('#translate');
-        
-        translate.addEventListener('click',()=>{
-
-            let inputTranslate = document.querySelector('#input-translate');
-            let texToTranslate = inputTranslate.value;
-
-            // pedir la traduccion
-            const encodedParams = new URLSearchParams();
-            encodedParams.append("source_language", source_language);
-            encodedParams.append("target_language", target_language);
-            encodedParams.append("text", texToTranslate);
-
-            const options = {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/x-www-form-urlencoded',
-                    'X-RapidAPI-Key': '6930271f20msh2fab5da401100b2p1c90d9jsn28f3d04b3f89',
-                    'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
-                },
-                body: encodedParams
-            };
-
-            fetch('https://text-translator2.p.rapidapi.com/translate', options)
-                .then(response => response.json())
-                .then(response => translateOut.value=response.data.translatedText)
-                .catch(err => console.error(err));*/
-});
-selectTag.forEach(tag=>{
-    for (const key in object) {
-        if (Object.hasOwnProperty.call(object, key)) {
-            const element = object[key];
-            
-        }
-    }
+    fetch(URL_API).then(res => res.json()).then(data=>{
+        ToText.value = data.responseData.translatedText;
+    })
 });
