84a2732036431b8d9f2b5bdcd177886209820796
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-02-28 18:55:54 -0500

    working on con speech text

--- js/main.js
+++ js/main.js
@@ -1,84 +1,3 @@
-/*
-// elementos dDOM
-let translateFrom = document.querySelector('#selectLanguagesFrom');
-let translateTo = document.querySelector('#selectLanguagesTo');
-
-//Consultar lista de lenguajes desde el servidor
-const GET_URL = 'https://text-translator2.p.rapidapi.com/getLanguages';
-
-//Encabezados
-const OPTIONS={
-    method:'get',
-    headers: {
-        'X-RapidAPI-Key': '6930271f20msh2fab5da401100b2p1c90d9jsn28f3d04b3f89',
-        'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
-    }
-}
-
-//Enviar listado de lenguajes disponibles
-//then resolver promesas
-fetch(GET_URL,OPTIONS)
-    .then(res=>res.json())
-    .then(objeto =>{
-        let languages = objeto.data.languages;
-        //console.log()
-        //Codigo necesario para cargar el select
-        languages.forEach(element => {
-            translateFrom.innerHTML += `<option value="${element.code}">${element.name}</option>`
-            translateTo.innerHTML += `<option value="${element.code}">${element.name}</option>`
-        });
-        // tomar valor del translate from
-        translateFrom.addEventListener('click',()=>{
-                //console.log(translateFrom.value);
-                source_language=translateFrom.value;
-            });
-
-            // tomar valor del translate to
-            translateTo.addEventListener('click',()=>{
-                //console.log(translateTo.value);
-                target_language = translateTo.value;
-            });
-
-        }).catch(error => console.log(error));
-
-        // almacenar valores a traducir
-        let source_language = 'es';
-        let target_language = 'af'
-
-        //mostrar resultado
-        let translateOut = document.querySelector('#output-translate');
-        //Obtener datos del input para enviar al servidor
-        let translate = document.querySelector('#translate');
-        
-        translate.addEventListener('click',()=>{
-
-            let inputTranslate = document.querySelector('#input-translate');
-            let texToTranslate = inputTranslate.value;
-
-            // pedir la traduccion
-            const encodedParams = new URLSearchParams();
-            encodedParams.append("source_language", source_language);
-            encodedParams.append("target_language", target_language);
-            encodedParams.append("text", texToTranslate);
-
-            const options = {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/x-www-form-urlencoded',
-                    'X-RapidAPI-Key': '6930271f20msh2fab5da401100b2p1c90d9jsn28f3d04b3f89',
-                    'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
-                },
-                body: encodedParams
-            };
-
-            fetch('https://text-translator2.p.rapidapi.com/translate', options)
-                .then(response => response.json())
-                .then(response => translateOut.value=response.data.translatedText)
-                .catch(err => console.error(err));
-});
-*/
-/*---------------------------------VERSION 2 ---------------*/
-
 // Tags
 console.log("Hola, bienvenido!")
 const selectTag = document.querySelectorAll("select"),
@@ -97,6 +16,8 @@ let translateTo = document.querySelector('.select-to');
 // Valores predeterminados
 let source_language = 'af';
 let target_language = 'af';
+let name_source = "Afrikaans"
+let name_target = "Afrikaans"
 
 
 
@@ -121,7 +42,6 @@ fetch(GET_URL,OPTIONS)
     .then(res=>res.json())
     .then(objeto =>{
         languages = objeto.data.languages;
-        
         loadLanguages(languages)
 
 }).catch(error => console.log(error));
@@ -146,13 +66,22 @@ function loadLanguages(languages){
     translateFrom.addEventListener('click',()=>{
         //console.log(translateFrom.value);
         source_language = translateFrom.value;
+        languages.find(function(name){
+            if(name.code === source_language){
+                name_source= name.name;
+            }
+        });
     });
     
      // obtener valor del to, para luego mandar a la API
     translateTo.addEventListener('click',()=>{
         //console.log(translateTo.value);
         target_language = translateTo.value;
-        
+        languages.find(function(name){
+            if(name.code === target_language){
+                name_target = name.name;
+            }
+        });
     });
 
     // Cambiar
@@ -182,7 +111,7 @@ function loadLanguages(languages){
             translateFrom[indiceSeleccionado].innerHTML= translateTo[0].innerHTML
             translateTo[0].innerHTML =  innerSeleccionado;
 
-            let tempText = fromText.value
+            let tempText = fromText.value;
             fromText.value = ToText.value
             ToText.value = tempText
             
@@ -224,10 +153,10 @@ icons.forEach(icon=>{
             let utterance;
             if(target.id == "from"){
                utterance = new SpeechSynthesisUtterance(fromText.value);
-               utterance.lang = "English"
+               utterance.lang = name_source;
             } else{
                 utterance = new SpeechSynthesisUtterance(ToText.value);
-                utterance.lang = "English"
+                utterance.lang = name_target
             }
             speechSynthesis.speak(utterance)
         }
